a0d227a9381722e4fa59f5a7739dbc7a750f611f
Sky <wilson.skylerm@gmail.com>
2017-11-03 15:40:26 -0700

    Added basic location permissions prompt

--- app/components/SubmitButton.js
+++ app/components/SubmitButton.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { StyleSheet, View, Button } from 'react-native';
+import { PermissionsAndroid } from 'react-native';
 
 var SQLite = require('react-native-sqlite-storage')
 
@@ -9,13 +10,33 @@ export default class SubmitButton extends Component<{}>{
 		this.state = {
 			buttonText: "Submit Message"
 		}
-	}
-
-	pressSubmit = () => {
-        	db = SQLite.openDatabase('android-sqliteNew.db', SQLite.OPEN_READWRITE);
+        	db = SQLite.openDatabase('android-sqlite.db', SQLite.OPEN_READWRITE);
 		db.executeSql('CREATE TABLE IF NOT EXISTS Messages( '
 			+ 'geolocation FLOAT, '
 			+ 'text VARCHAR(55));');
+	}
+
+	pressSubmit = () => {
+		this.requestLocationPermission();
+	}
+
+	async requestLocationPermission(){
+		try {
+			const granted = await PermissionsAndroid.request(
+				PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+				{
+					'title': 'Test Permission Prompt',
+					'message': 'Can this app have access to your location?' 
+				}
+			)
+			if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+				alert("You can use the location")
+			} else {
+				alert("Camera permission location")
+			}
+		} catch (err) {
+			console.warn(err)
+		}
 	}
 
 	render(){
